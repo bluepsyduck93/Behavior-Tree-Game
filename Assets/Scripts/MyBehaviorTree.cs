f9fd44fe77f03f1fe2dd5d08be6af46ac30415a4
bluepsyduck93 <ccblue93@gmail.com>
2019-05-14 06:41:55 -0400

    Part 1 works except for staffswap . Testing IK

--- Assets/Scripts/MyBehaviorTree.cs
+++ Assets/Scripts/MyBehaviorTree.cs
@@ -2,27 +2,28 @@
 using System;
 using System.Collections;
 using TreeSharpPlus;
+using RootMotion.FinalIK;
 
 public class MyBehaviorTree : MonoBehaviour
 {
-    public GameObject cube1;
-    public GameObject cube2;
-    //public GameObject participant1;
-    //public GameObject participant2;
-    //public GameObject participant3;
     GameObject staff;
     GameObject cult1;
     GameObject cult2;
     GameObject cult3;
     GameObject flash;
     GameObject vampire;
+    GameObject player;
+    GameObject clue1;
+    GameObject cube;
     bool summonactive;
+    bool evil;
     float _t = 0f;
 
     private BehaviorAgent behaviorAgent;
     // Use this for initialization
     void Start()
     {
+        /*Intializing the game Objects*/
         vampire = GameObject.FindGameObjectWithTag("vampire");
         vampire.SetActive(false);
         flash = GameObject.FindGameObjectWithTag("light");
@@ -31,6 +32,9 @@ public class MyBehaviorTree : MonoBehaviour
         cult1 = GameObject.FindGameObjectWithTag("cult1");
         cult2 = GameObject.FindGameObjectWithTag("cult2");
         cult3 = GameObject.FindGameObjectWithTag("cult3");
+        player = GameObject.FindGameObjectWithTag("player");
+        cube = GameObject.FindGameObjectWithTag("cube");
+        clue1 = GameObject.FindGameObjectWithTag("clue1");
         behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
         BehaviorManager.Instance.Register(behaviorAgent);
         behaviorAgent.StartBehavior();
@@ -54,16 +58,36 @@ public class MyBehaviorTree : MonoBehaviour
             vampire.SetActive(false);
             staff.SetActive(true);
         }
+        /*Evil Timer Start*/
+        _t += Time.deltaTime;
+        if (_t >= 10.0)
+        {
+            _t = 0f;
+            changeCubeColor();
+        }
+        /*Evil Timer End*/
     }
     protected Node BuildTreeRoot()
     {
-        return new DecoratorLoop(
-                new Sequence(
-                    this.MoveCultRoot()
-                )
-            );
+        /*DO NOT EDIT START*/
+        return
+            new Sequence(
+            new SequenceParallel(
+            MoveCultRoot(), ObjFloats(staff),
+            PraiseCultRoot()),
+            AssertFearCultRoot(),
+
+        /*DO NOT EDIT END*/
+          /* this.staffSwap(),*/
+           startPlayer()
+        /*ADD OTHER NODES BELOW LIKE THIS -> , MyNode1(), MyNode2() */   
+           );
+
     }
-    protected Node AssertAndDie(GameObject currentPerson)
+
+    /*Death Functions (Will Implement Later)*/
+
+    /*protected Node AssertAndDie(GameObject currentPerson)
     {
         return new DecoratorLoop(new Sequence(new DecoratorInvert(new DecoratorLoop((new DecoratorInvert(new Sequence(this.CheckDead(currentPerson, cube2)))))),
                                               currentPerson.GetComponent<BehaviorMecanim>().ST_PlayBodyGesture("DYING",10000)));
@@ -78,96 +102,111 @@ public class MyBehaviorTree : MonoBehaviour
         return new LeafAssert(() => (distaway.Value < 5.0));
 
 
-    }
+    }*/
 
+    /*DO NOT EDIT START*/
+
+    /*Cultists are Praising Here*/
     protected Node MoveCultRoot()
     {
-        return new DecoratorLoop(new DecoratorInvert(
-                                 new DecoratorLoop(
-                                (new SequenceParallel(WalkToCube(cult1), 
-                                                      WalkToCube(cult2), 
-                                                      WalkToCube(cult3))))));
+        return new SequenceParallel(WalkToObj(cult1), 
+                                                      WalkToObj(cult2), 
+                                                      WalkToObj(cult3));
     }
     protected Node PraiseCultRoot()
     {
-        return new SequenceParallel(new LeafWait(1000),
+        return new SequenceParallel(new LeafWait(100),
                                                       ParticipantPraise(cult1),
                                                       ParticipantPraise(cult2),
                                                       ParticipantPraise(cult3));
     }
-    
-    protected Node FearCultRoot()
+    protected Node WalkToObj(GameObject currentPerson)
     {
-        return new DecoratorLoop(new DecoratorInvert(
-                                 new DecoratorLoop(
-                                 new SequenceParallel(new LeafWait(1000),
-                                                      ParticipantFear(cult1),
-                                                      ParticipantFear(cult2),
-                                                      ParticipantFear(cult3)))));
-    }
-
-    protected Node CubeFloats(GameObject cube)
-    {
-        Vector3 lowestPosition = cube.transform.position;
-
-        Vector3 highestPosition = lowestPosition + new Vector3 (0, 3, 0);
-
-        return new DecoratorLoop(new Sequence(new LeafInvoke(()=> StartCoroutine(ApproachObj(cube1,lowestPosition, highestPosition))),
-                                              new LeafWait(10500),
-                                              new LeafInvoke(() => StartCoroutine(ApproachObj(cube1, highestPosition, lowestPosition))),
-                                              new LeafWait(10500)));
-
-    }
-    protected Node WalkToCube(GameObject currentPerson)
-    {
-        Vector3 cubeposition = new Vector3(staff.transform.position.x, 0f, staff.transform.position.z);
-        return new Sequence(new LeafInvoke(()=> currentPerson.GetComponent<SteeringController>().Target = (cubeposition)));
+        Vector3 staffposition = new Vector3(staff.transform.position.x, 0f, staff.transform.position.z);
+        return new Sequence(currentPerson.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(staffposition, 1.0f));
     }
     protected Node ParticipantPraise(GameObject currentPerson)
     {
-        return new Sequence(currentPerson.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("CLAP", 10000), new LeafWait(1000));
+        return new Sequence(currentPerson.GetComponent<BehaviorMecanim>().ST_PlayGesture("CLAP", AnimationLayer.Hand, 10000));
     }
-    protected Node ParticipantFear(GameObject currentPerson)
+
+    /* Staff Floating */
+    protected Node ObjFloats(GameObject obj)
     {
-        return new Sequence(currentPerson.GetComponent<BehaviorMecanim>().ST_PlayBodyGesture("DUCK", 10000), new LeafWait(1000));
+        Vector3 lowestPosition = obj.transform.position;
+
+        Vector3 highestPosition = lowestPosition + new Vector3 (0, 3, 0);
+
+        return new Sequence(new LeafInvoke(()=> StartCoroutine(ApproachObj(obj,lowestPosition, highestPosition))),
+                                              new LeafWait(1000),
+                                              new LeafInvoke(() => StartCoroutine(ApproachObj(obj, highestPosition, lowestPosition))),
+                                              new LeafWait(1000));
+
     }
-    protected Node summon(GameObject cult1, GameObject cult2, GameObject cult3)
-    {
-        return new Sequence(
-                //participantpraise,
-                ParticipantPraise(cult1),
-                ParticipantPraise(cult2),
-                ParticipantPraise(cult3),
-                //appearance,
-                staffSwap(),
-                //create new sequenceshuffle
-                new SequenceParallel(
-                        ParticipantFear(cult1),
-                        RunAway(cult2),
-                        AssertAndDie(cult3)
-                    )
-            );
-    }
-    protected Node desummon()
-    {
-        summonactive = false;
-    }
-    protected Node staffSwap()
-    {
-        summonactive = true;
-    }
-    IEnumerator ApproachObj(GameObject cube, Vector3 start, Vector3 end)
+    IEnumerator ApproachObj(GameObject obj, Vector3 start, Vector3 end)
     {
         float startTime = Time.time;
         while (Time.time < startTime + 10f)
         {
-            cube.transform.position = Vector3.Lerp(start, end, (Time.time - startTime)/10f);
+            obj.transform.position = Vector3.Lerp(start, end, (Time.time - startTime) / 10f);
             yield return null;
         }
-        cube.transform.position = end;
-        
+        obj.transform.position = end;
+
 
     }
+    /*Evil Starts Here*/
+    void changeCubeColor()
+    {
+        cube.transform.GetComponent<Renderer>().material.color = Color.red;
 
+        evil = true;
+    }
+
+    protected Node AssertFearCultRoot()
+    {
+        return
+               new Sequence(
+               new DecoratorInvert(
+               new DecoratorLoop((new DecoratorInvert(new Sequence(this.CheckEvil()))))),
+               new SequenceParallel(ParticipantFear(cult1), ParticipantFear(cult2), ParticipantFear(cult3)));
+    }
+
+    protected Node CheckEvil()
+    {
+        return new LeafAssert(() => evil);
+    }
+
+    protected Node ParticipantFear(GameObject currentPerson)
+    {
+        return new Sequence(currentPerson.GetComponent<BehaviorMecanim>().ST_PlayGesture("DUCK", AnimationLayer.Body, 10000));
+    }
+    /*DO NOT EDIT END*/
+
+    /*Demon + Staff Mechanism*/
+    protected Node desummon()
+    {
+        return new LeafAssert(() => summonactive = false);
+    }
+    protected Node staffSwap()
+    {
+        return new LeafAssert(()=>summonactive = true);
+    }
+
+
+    protected Node startPlayer()
+    {
+        return new Sequence(playerPickUp());
+    }
+   protected Node playerPickUp()
+    {
+        Vector3 clueposition = new Vector3(clue1.transform.position.x, clue1.transform.position.y, clue1.transform.position.z);
+        Val<InteractionObject> clue = Val.V(() => { return this.clue1.GetComponent<InteractionObject>(); });
+        Val<FullBodyBipedEffector> right = FullBodyBipedEffector.RightHand;
+
+       return new Sequence(player.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(clueposition, 1.0f)
+           /*,
+           player.GetComponent<BehaviorMecanim>().Node_StartInteraction(right, clue)*/);
+    }
 
 }
